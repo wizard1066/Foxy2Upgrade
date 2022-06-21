1a084b59e1dc4179096b110aee8a917cc045ee63
wizard1066 <mark.lucking@gmail.com>
2022-06-21 12:44:56 +0200

    Initial Commit

---
+++ Foxy2Upgrade/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Foxy2Upgrade
+//
+//  Created by localuser on 21.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Foxy2Upgrade/Foxy2UpgradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foxy2UpgradeApp.swift
+//  Foxy2Upgrade
+//
+//  Created by localuser on 21.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Foxy2UpgradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
